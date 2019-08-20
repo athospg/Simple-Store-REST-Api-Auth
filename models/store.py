6620fddfa3c67b068a465435494dbd03a5ae4e2b
Athos Garcia <athospg@outlook.com>
2019-08-20 09:26:15 -0300

    Added relationship between Item and Store.

--- models/store.py
+++ models/store.py
@@ -7,3 +7,5 @@ class StoreModel(db.Model):
     # Columns in the database
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
+
+    items = db.relationship('ItemModel', lazy='dynamic', cascade="all, delete-orphan")
