94fe21789e7d388b9107316a0952ac52d6c824b9
Athos Garcia <athospg@outlook.com>
2019-08-20 10:00:27 -0300

    Added Unit test for Item creation.

--- tests/unit/models/test_item.py
+++ tests/unit/models/test_item.py
@@ -1,6 +1,18 @@
 from unittest import TestCase
 
+from models.item import ItemModel
+# noinspection PyUnresolvedReferences
+from models.store import StoreModel
+
 
 class ItemTest(TestCase):
     def test_create_item(self):
-        pass
+        # Setup
+
+        # Exercise
+        item = ItemModel('test', 13.99, 3)
+
+        # Verify
+        self.assertEqual('test', item.name)
+        self.assertEqual(13.99, item.price)
+        self.assertEqual(3, item.store_id)
