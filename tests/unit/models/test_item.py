ee45864666a51284c1e225a29bd3d96bec319795
Athos Garcia <athospg@outlook.com>
2019-08-21 09:03:47 -0300

    Added Unit test for item jsonify.

--- tests/unit/models/test_item.py
+++ tests/unit/models/test_item.py
@@ -18,4 +18,11 @@ class ItemTest(TestCase):
         self.assertEqual(3, item.store_id)
 
     def test_item_json(self):
-        pass
+        # Setup
+
+        # Exercise
+        item = ItemModel('Test Item', 1.99, 1)
+
+        # Verify
+        expected = {'id': None, 'name': 'Test Item', 'price': 1.99, 'store_id': 1}
+        self.assertEqual(expected, item.json())
