b37c30da570e3d41da5e6999e3132cd380a7dd84
Athos Garcia <athospg@outlook.com>
2019-08-21 09:43:17 -0300

    Added Item model function to delete Item from database.

--- models/item.py
+++ models/item.py
@@ -32,3 +32,7 @@ class ItemModel(db.Model):
     def save_to_db(self):
         db.session.add(self)
         db.session.commit()
+
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
