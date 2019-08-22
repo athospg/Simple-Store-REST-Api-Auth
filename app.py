ab1d7ee4c1d463503bfc9ec2abf551f4df1946d6
Athos Garcia <athospg@outlook.com>
2019-08-22 09:28:54 -0300

    Update app with database file, resources and some Flask configurations.
    Fix Store resource class name.
    
    Added '.db' to gitignore.

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
 from flask import Flask
+from flask_restful import Api
+
+from resources.item import Item
+from resources.store import Store
 
 app = Flask(__name__)
+
+app.config['DEBUG'] = True
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['PROPAGATE_EXCEPTIONS'] = True
+
+api = Api(app)
+
+# Store related resources
+api.add_resource(Store, '/store/<string:name>')
+
+# Item related resources
+api.add_resource(Item, '/item/<string:name>')
 
 
 @app.route('/')
@@ -9,4 +27,12 @@ def hello_world():
 
 
 if __name__ == '__main__':
+    from db import db
+
+    if app.config['DEBUG']:
+        @app.before_first_request
+        def create_tables():
+            db.create_all()
+
+    db.init_app(app)
     app.run()
--- resources/store.py
+++ resources/store.py
@@ -3,7 +3,7 @@ from flask_restful import Resource
 from models.store import StoreModel
 
 
-class Item(Resource):
+class Store(Resource):
     def get(self, name: str):
         pass
 
