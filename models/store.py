7ad925be9118941b7c5ed552d010392cf36d218f
Athos Garcia <athospg@outlook.com>
2019-08-22 09:14:19 -0300

    Added Store model function to delete Store from database.

--- models/store.py
+++ models/store.py
@@ -27,3 +27,7 @@ class StoreModel(db.Model):
     def save_to_db(self):
         db.session.add(self)
         db.session.commit()
+
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
