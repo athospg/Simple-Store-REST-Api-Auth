e4a14b6efad11b2b96c8d7d5c2295c01097ea5ab
Athos Garcia <athospg@outlook.com>
2019-08-26 09:37:22 -0300

    Added CRUD integration test for Item.

--- tests/integration/models/test_item.py
+++ tests/integration/models/test_item.py
@@ -1,5 +1,33 @@
+from models.item import ItemModel
+from models.store import StoreModel
 from tests.base_test import BaseTest
 
 
 class ItemTest(BaseTest):
-    pass
+    def test_crud(self):
+        """
+        CRUD: Create, Read, Update and Delete.
+        These are the four major and basic functions of persistent storage.
+        """
+        with self.app_context():
+            # Setup
+            StoreModel('test').save_to_db()
+            item = ItemModel('test', 19.99, 1)
+
+            # Exercise
+
+            # Verify
+            self.assertIsNone(ItemModel.find_by_name('test'),
+                              "Found an item with name {}, but expected not to.".format(item.name))
+
+            # Exercise
+            item.save_to_db()
+
+            # Verify
+            self.assertIsNotNone(ItemModel.find_by_name('test'))
+
+            # Exercise
+            item.delete_from_db()
+
+            # Verify
+            self.assertIsNone(ItemModel.find_by_name('test'))
