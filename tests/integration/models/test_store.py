1b5bc1fe7ff8fd0d5739519d082b5692aeb14be4
Athos Garcia <athospg@outlook.com>
2019-08-26 09:51:40 -0300

    Added CRUD integration test for Store.

--- tests/integration/models/test_store.py
+++ tests/integration/models/test_store.py
@@ -1,5 +1,30 @@
+from models.store import StoreModel
 from tests.base_test import BaseTest
 
 
 class StoreTest(BaseTest):
-    pass
+    def test_crud(self):
+        """
+        CRUD: Create, Read, Update and Delete.
+        These are the four major and basic functions of persistent storage.
+        """
+        with self.app_context():
+            # Setup
+            store = StoreModel('test')
+
+            # Exercise
+
+            # Verify
+            self.assertIsNone(StoreModel.find_by_name('test'))
+
+            # Exercise
+            store.save_to_db()
+
+            # Verify
+            self.assertIsNotNone(StoreModel.find_by_name('test'))
+
+            # Exercise
+            store.delete_from_db()
+
+            # Verify
+            self.assertIsNone(StoreModel.find_by_name('test'))
