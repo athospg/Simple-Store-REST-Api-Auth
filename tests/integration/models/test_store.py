af4acdb7e2c160f99650dd00b1a59430823db4ac
Athos Garcia <athospg@outlook.com>
2019-08-26 10:23:01 -0300

    Added jsonify with Items integration test for Store.

--- tests/integration/models/test_store.py
+++ tests/integration/models/test_store.py
@@ -49,3 +49,21 @@ class StoreTest(BaseTest):
             # Verify
             self.assertEqual(1, store.items.count())
             self.assertEqual('Item A', store.items.first().name)
+
+    def test_store_json_with_item(self):
+        with self.app_context():
+            # Setup
+            store = StoreModel('test')
+            item = ItemModel('Item A', 19.99, 1)
+
+            # Exercise
+            store.save_to_db()
+            item.save_to_db()
+
+            # Verify
+            expected = {
+                'id': 1,
+                'name': 'test',
+                'items': [{'id': 1, 'name': 'Item A', 'price': 19.99, 'store_id': 1}]
+            }
+            self.assertDictEqual(expected, store.json())
