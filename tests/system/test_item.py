b456501ec2dcfd0c4478a205ad8f003556eee815
Athos Garcia <athospg@outlook.com>
2019-08-27 09:33:48 -0300

    Added system test get Item not found.

--- tests/system/test_item.py
+++ tests/system/test_item.py
@@ -2,4 +2,15 @@ from tests.base_test import BaseTest
 
 
 class ItemTest(BaseTest):
-    pass
+
+    def test_get_item_not_found(self):
+        with self.app() as client:
+            with self.app_context():
+                # Setup
+
+                # Exercise
+                path = '/item/test'
+                resp = client.get(path)
+
+                # Verify
+                self.assertEqual(404, resp.status_code)
