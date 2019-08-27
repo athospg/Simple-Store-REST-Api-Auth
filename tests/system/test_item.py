23e99f2c6d68bb44469702940b947d2daefaea33
Athos Garcia <athospg@outlook.com>
2019-08-27 09:48:34 -0300

    Added system test create duplicated Item function.

--- tests/system/test_item.py
+++ tests/system/test_item.py
@@ -5,6 +5,7 @@ from models.store import StoreModel
 from tests.base_test import BaseTest
 
 
+# noinspection DuplicatedCode
 class ItemTest(BaseTest):
 
     def test_get_item_not_found(self):
@@ -52,4 +53,23 @@ class ItemTest(BaseTest):
                 expected = {'id': 1, 'name': 'test', 'price': 17.99, 'store_id': 1}
 
                 self.assertEqual(201, resp.status_code)
+                self.assertDictEqual(expected, json.loads(resp.data))
+
+    def test_create_duplicate_item(self):
+        with self.app() as client:
+            with self.app_context():
+                # Setup
+                StoreModel('test').save_to_db()
+                ItemModel('test', 17.99, 1).save_to_db()
+
+                # Exercise
+                path = '/item/test'
+                headers = {'Content-Type': 'application/json'}
+                data = json.dumps({'price': 17.99, 'store_id': 1})
+                resp = client.post(path, headers=headers, data=data)
+
+                # Verify
+                expected = {'message': 'An item with name \'test\' already exists.'}
+
+                self.assertEqual(400, resp.status_code)
                 self.assertDictEqual(expected, json.loads(resp.data))
