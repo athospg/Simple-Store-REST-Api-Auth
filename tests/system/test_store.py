6852c3657561d4710f18ef056818d7c917863920
Athos Garcia <athospg@outlook.com>
2019-08-27 10:11:22 -0300

    Added system test get Store function.

--- tests/system/test_store.py
+++ tests/system/test_store.py
@@ -1,5 +1,6 @@
 import json
 
+from models.store import StoreModel
 from tests.base_test import BaseTest
 
 
@@ -18,4 +19,20 @@ class StoreTest(BaseTest):
                 expected = {'message': 'Store not found'}
 
                 self.assertEqual(404, resp.status_code)
+                self.assertDictEqual(expected, json.loads(resp.data))
+
+    def test_get_store(self):
+        with self.app() as client:
+            with self.app_context():
+                # Setup
+                StoreModel('test').save_to_db()
+
+                # Exercise
+                path = '/store/test'
+                resp = client.get(path)
+
+                # Verify
+                expected = {'id': 1, 'name': 'test', 'items': []}
+
+                self.assertEqual(200, resp.status_code)
                 self.assertDictEqual(expected, json.loads(resp.data))
