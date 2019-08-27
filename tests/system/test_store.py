16eb1b0054c278777ee029d6deb9a59875866d3f
Athos Garcia <athospg@outlook.com>
2019-08-27 10:09:48 -0300

    Added system test get Store not found function.

--- tests/system/test_store.py
+++ tests/system/test_store.py
@@ -1,5 +1,21 @@
+import json
+
 from tests.base_test import BaseTest
 
 
 class StoreTest(BaseTest):
-    pass
+
+    def test_get_store_not_found(self):
+        with self.app() as client:
+            with self.app_context():
+                # Setup
+
+                # Exercise
+                path = '/store/test'
+                resp = client.get(path)
+
+                # Verify
+                expected = {'message': 'Store not found'}
+
+                self.assertEqual(404, resp.status_code)
+                self.assertDictEqual(expected, json.loads(resp.data))
