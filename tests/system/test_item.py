5c427571d36216202b5a5472ff9823e655a56fb6
Athos Garcia <athospg@outlook.com>
2019-08-27 09:37:08 -0300

    Added system test get Item function.

--- tests/system/test_item.py
+++ tests/system/test_item.py
@@ -1,3 +1,7 @@
+import json
+
+from models.item import ItemModel
+from models.store import StoreModel
 from tests.base_test import BaseTest
 
 
@@ -14,3 +18,20 @@ class ItemTest(BaseTest):
 
                 # Verify
                 self.assertEqual(404, resp.status_code)
+
+    def test_get_item(self):
+        with self.app() as client:
+            with self.app_context():
+                # Setup
+                StoreModel('test').save_to_db()
+                ItemModel('test', 19.99, 1).save_to_db()
+
+                # Exercise
+                path = '/item/test'
+                resp = client.get(path)
+
+                # Verify
+                expected = {'id': 1, 'name': 'test', 'price': 19.99, 'store_id': 1}
+
+                self.assertEqual(200, resp.status_code)
+                self.assertDictEqual(expected, json.loads(resp.data))
