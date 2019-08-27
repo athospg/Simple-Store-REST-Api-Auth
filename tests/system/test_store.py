5f4ad624ada4b9669c8a05eb053b173accd84ba0
Athos Garcia <athospg@outlook.com>
2019-08-27 10:41:23 -0300

    Added system test get Store list with Items function.

--- tests/system/test_store.py
+++ tests/system/test_store.py
@@ -124,3 +124,33 @@ class StoreTest(BaseTest):
                     {'id': 2, 'name': 'Store B', 'items': []}
                 ]}
                 self.assertDictEqual(expected, json.loads(resp.data))
+
+    def test_get_store_list_with_items(self):
+        with self.app() as client:
+            with self.app_context():
+                # Setup
+                StoreModel('Store A').save_to_db()
+                ItemModel('Item A1', 5.99, 1).save_to_db()
+                ItemModel('Item A2', 1.99, 1).save_to_db()
+                StoreModel('Store B').save_to_db()
+                ItemModel('Item B1', 9.99, 2).save_to_db()
+
+                # Exercise
+                path = '/stores'
+                resp = client.get(path)
+
+                # Verify
+                expected = {'stores': [
+                    {
+                        'id': 1, 'name': 'Store A',
+                        'items': [
+                            {'id': 1, 'name': 'Item A1', 'price': 5.99, 'store_id': 1},
+                            {'id': 2, 'name': 'Item A2', 'price': 1.99, 'store_id': 1}
+                        ]
+                    }, {
+                        'id': 2, 'name': 'Store B',
+                        'items': [{'id': 3, 'name': 'Item B1', 'price': 9.99, 'store_id': 2}]
+                    }
+                ]}
+
+                self.assertDictEqual(expected, json.loads(resp.data))
