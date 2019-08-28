9c7ed47a4476bea147c344a4f17eef9acf11539f
Athos Garcia <athospg@outlook.com>
2019-08-28 09:11:34 -0300

    Add init function to UserModel class.

--- models/user.py
+++ models/user.py
@@ -8,3 +8,7 @@ class UserModel(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(80))
     password = db.Column(db.String(80))
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
