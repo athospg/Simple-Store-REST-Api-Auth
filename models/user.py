497680405f092842129e092efab2d39dd34ac27a
Athos Garcia <athospg@outlook.com>
2019-08-28 09:37:13 -0300

    Added function to find user by id.

--- models/user.py
+++ models/user.py
@@ -27,6 +27,10 @@ class UserModel(db.Model):
     def find_by_username(cls, username):
         return cls.query.filter_by(username=username).first()
 
+    @classmethod
+    def find_by_id(cls, _id):
+        return cls.query.filter_by(id=_id).first()
+
     def delete_from_db(self):
         db.session.delete(self)
         db.session.commit()
