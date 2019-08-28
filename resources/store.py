ed5dc13ef02fbe043d39542fd9424a524066b377
Athos Garcia <athospg@outlook.com>
2019-08-28 10:57:03 -0300

    Set User authorization for Store access.

--- resources/store.py
+++ resources/store.py
@@ -1,15 +1,18 @@
+from flask_jwt_extended import jwt_required, fresh_jwt_required
 from flask_restful import Resource
 
 from models.store import StoreModel
 
 
 class Store(Resource):
+    @jwt_required
     def get(self, name: str):
         store = StoreModel.find_by_name(name)
         if store:
             return store.json(), 200
         return {'message': 'Store not found'}, 404
 
+    @jwt_required
     def post(self, name):
         if StoreModel.find_by_name(name):
             return {'message': "A store with name '{}' already exists.".format(name)}, 400
@@ -22,11 +25,13 @@ class Store(Resource):
 
         return store.json(), 201
 
+    @jwt_required
     def put(self, name):
         # For now the Store has only an unique name and no other properties.
         # TODO: Change this function when the Store has more properties.
         return self.post(name)
 
+    @fresh_jwt_required
     def delete(self, name):
         store = StoreModel.find_by_name(name)
         if store:
@@ -37,5 +42,6 @@ class Store(Resource):
 
 
 class StoreList(Resource):
+    @jwt_required
     def get(self):
         return {'stores': [store.json() for store in StoreModel.find_all()]}, 200
