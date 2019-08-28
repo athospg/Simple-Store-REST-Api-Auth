61c4247ec60eac7866309e5c43432e497c451304
Athos Garcia <athospg@outlook.com>
2019-08-28 09:50:38 -0300

    Added CRUD integration test for User.

--- tests/integration/models/test_user.py
+++ tests/integration/models/test_user.py
@@ -1,5 +1,33 @@
+from models.user import UserModel
 from tests.base_test import BaseTest
 
 
 class UserTest(BaseTest):
-    pass
+    def test_crud(self):
+        """
+        CRUD: Create, Read, Update and Delete.
+        These are the four major and basic functions of persistent storage.
+        """
+        with self.app_context():
+            # Setup
+            user = UserModel('test', 'abcd')
+
+            # Exercise
+
+            # Verify
+            self.assertIsNone(UserModel.find_by_username('test'))
+            self.assertIsNone(UserModel.find_by_id(1))
+
+            # Exercise
+            user.save_to_db()
+
+            # Verify
+            self.assertIsNotNone(UserModel.find_by_username('test'))
+            self.assertIsNotNone(UserModel.find_by_id(1))
+
+            # Exercise
+            user.delete_from_db()
+
+            # Verify
+            self.assertIsNone(UserModel.find_by_username('test'))
+            self.assertIsNone(UserModel.find_by_id(1))
