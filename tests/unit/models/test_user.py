35fe2394e3b38bc5c8a23c98bc19a5126456382e
Athos Garcia <athospg@outlook.com>
2019-08-28 09:16:53 -0300

    Added User creation Unit test.

--- tests/unit/models/test_user.py
+++ tests/unit/models/test_user.py
@@ -1,5 +1,15 @@
 from unittest import TestCase
 
+from models.user import UserModel
+
 
 class UserTest(TestCase):
-    pass
+    def test_create_user(self):
+        # Setup
+
+        # Exercise
+        user = UserModel('test', 'abcd')
+
+        # Verify
+        self.assertEqual('test', user.username)
+        self.assertEqual('abcd', user.password)
