6cb9677466402f3f9657f900d0d0d5012a3d7a30
Athos Garcia <athospg@outlook.com>
2019-08-28 10:53:13 -0300

    Added User token refresh resource function.

--- resources/user.py
+++ resources/user.py
@@ -1,4 +1,7 @@
-from flask_jwt_extended import jwt_required, create_access_token, create_refresh_token
+from flask_jwt_extended import (
+    jwt_required, create_access_token, create_refresh_token, jwt_refresh_token_required, get_jwt_identity,
+    fresh_jwt_required
+)
 from flask_restful import Resource, reqparse
 
 from models.user import UserModel
@@ -64,7 +67,7 @@ class User(Resource):
         return {'message': 'User not found'}, 404
 
     @classmethod
-    @jwt_required
+    @fresh_jwt_required
     def delete(cls, user_id: int):
         user = UserModel.find_by_id(user_id)
         if user:
@@ -75,3 +78,11 @@ class User(Resource):
 
 class UserLogout(Resource):
     pass
+
+
+class TokenRefresh(Resource):
+    @jwt_refresh_token_required
+    def post(self):
+        current_user = get_jwt_identity()
+        new_token = create_access_token(identity=current_user, fresh=False)
+        return {'access_token': new_token}, 200
