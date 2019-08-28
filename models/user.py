b9d4bc107f5ef8085b2a91934fa80c395024d06e
Athos Garcia <athospg@outlook.com>
2019-08-28 09:09:44 -0300

    Defined UserModel columns and table name.

--- models/user.py
+++ models/user.py
@@ -2,4 +2,9 @@ from db import db
 
 
 class UserModel(db.Model):
-    pass
+    __tablename__ = 'users'
+
+    # Columns in the database
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(80))
+    password = db.Column(db.String(80))
