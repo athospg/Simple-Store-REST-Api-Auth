d502b5fed4d6e9aea4c7ad07bbbc16c3b098d42b
Athos Garcia <athospg@outlook.com>
2019-08-28 09:59:07 -0300

    Added User resource with basic classes.

---
+++ resources/user.py
@@ -0,0 +1,17 @@
+from flask_restful import Resource
+
+
+class UserRegister(Resource):
+    pass
+
+
+class UserLogin(Resource):
+    pass
+
+
+class User(Resource):
+    pass
+
+
+class UserLogout(Resource):
+    pass
