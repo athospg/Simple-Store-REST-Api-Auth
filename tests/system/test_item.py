725aa2f8c0bafb62c749f7819dfaf163579f4f4a
Athos Garcia <athospg@outlook.com>
2019-08-29 10:18:23 -0300

    Added get Item without auth system test

--- tests/system/test_item.py
+++ tests/system/test_item.py
@@ -25,6 +25,18 @@ class ItemTest(BaseTest):
                 self.access_token = f'Bearer {access_token}'
                 self.refresh_token = f'Bearer {refresh_token}'
 
+    def test_get_item_no_auth(self):
+        with self.app() as client:
+            with self.app_context():
+                # Setup
+
+                # Exercise
+                path = '/item/test'
+                resp = client.get(path)
+
+                # Verify
+                self.assertEqual(401, resp.status_code)
+
     def test_get_item_not_found(self):
         with self.app() as client:
             with self.app_context():
