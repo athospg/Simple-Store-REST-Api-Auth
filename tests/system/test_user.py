7eb6c20f5814c2f3279e4e18848c60bed853855d
Athos Garcia <athospg@outlook.com>
2019-08-29 09:17:37 -0300

    Added duplicated User register system test.

--- tests/system/test_user.py
+++ tests/system/test_user.py
@@ -23,6 +23,24 @@ class UserNonLoggedTest(BaseTest):
                 self.assertDictEqual(expected, json.loads(response.data))
                 self.assertIsNotNone(UserModel.find_by_username('test'))
 
+    def test_register_duplicate_user(self):
+        with self.app() as client:
+            with self.app_context():
+                # Setup
+                UserModel('test', '1234').save_to_db()
+
+                # Exercise
+                path = '/register'
+                headers = {'Content-Type': 'application/json'}
+                data = json.dumps({'username': 'test', 'password': '1234'})
+                response = client.post(path, headers=headers, data=data)
+
+                # Verify
+                expected = {'message': 'A user with that username already exists.'}
+
+                self.assertEqual(400, response.status_code)
+                self.assertDictEqual(expected, json.loads(response.data))
+
 
 class UserLoggedTest(BaseTest):
     pass
