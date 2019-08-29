ce6760decf07375109afd2d3b183cffda66acf18
Athos Garcia <athospg@outlook.com>
2019-08-29 09:12:24 -0300

    Added User register system test.

--- tests/system/test_user.py
+++ tests/system/test_user.py
@@ -1,8 +1,27 @@
+import json
+
+from models.user import UserModel
 from tests.base_test import BaseTest
 
 
 class UserNonLoggedTest(BaseTest):
-    pass
+    def test_register_user(self):
+        with self.app() as client:
+            with self.app_context():
+                # Setup
+
+                # Exercise
+                path = '/register'
+                headers = {'Content-Type': 'application/json'}
+                data = json.dumps({'username': 'test', 'password': '1234'})
+                response = client.post(path, headers=headers, data=data)
+
+                # Verify
+                expected = {'message': 'User created successfully.'}
+
+                self.assertEqual(201, response.status_code)
+                self.assertDictEqual(expected, json.loads(response.data))
+                self.assertIsNotNone(UserModel.find_by_username('test'))
 
 
 class UserLoggedTest(BaseTest):
