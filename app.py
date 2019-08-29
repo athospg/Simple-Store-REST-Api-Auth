8b83e86dd7821b00400169f82e7016a5f63cfb6f
Athos Garcia <athospg@outlook.com>
2019-08-29 09:04:28 -0300

    Personalized error messages.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask
+from flask import Flask, jsonify
 from flask_jwt_extended import JWTManager
 from flask_restful import Api
 
@@ -34,6 +34,47 @@ def check_if_token_in_blacklist(decrypted_token):
         app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']  # To block both cases
     """
     return decrypted_token['jti'] in BLACKLIST
+
+
+# Personalized error messages
+@jwt.expired_token_loader
+def expired_token_callback():
+    return jsonify({
+        'description': 'The token has expired.',
+        'error': 'token_expired'
+    }), 401
+
+
+@jwt.invalid_token_loader
+def invalid_token_callback(error):
+    return jsonify({
+        'description': 'Signature verification failed.',
+        'error': 'invalid_token'
+    }), 401
+
+
+@jwt.unauthorized_loader
+def missing_token_callback(error):
+    return jsonify({
+        'description': 'Request does not contain an access token.',
+        'error': 'authorization_required'
+    }), 401
+
+
+@jwt.needs_fresh_token_loader
+def token_not_fresh_callback():
+    return jsonify({
+        'description': 'The token is not fresh.',
+        'error': 'fresh_token_required'
+    }), 401
+
+
+@jwt.revoked_token_loader
+def revoked_token_callback():
+    return jsonify({
+        'description': 'The token has been revoked.',
+        'error': 'token_removed'
+    }), 401
 
 
 # User related resources
